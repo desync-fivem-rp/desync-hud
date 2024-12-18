43e9cc7f8f38c0e88a5f5c72eebae054ca6cb573
hickeyli <hickeyli@msu.edu>
2024-12-18 16:38:34 -0500

    init

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+-- fixedWhileBuckled - Don't allow the person to jump/get out of the car while seatbelt is on
+Config.fixedWhileBuckled = true
+
+-- playSound - Should a buckle/unbuckle sound be played
+Config.playSound = true
+Config.seatbeltVolume = 0.5
+
+Config.toggleSeatbeltKey = "B" -- B
---
+++ fxmanifest.lua
@@ -0,0 +1,47 @@
+fx_version "cerulean"
+
+description "Desync HUD"
+author "Desync"
+version '1.0.0'
+
+lua54 'yes'
+
+games {
+  "gta5",
+  "rdr3"
+}
+
+ui_page 'web/build/index.html'
+
+shared_scripts {
+  '@ox_lib/init.lua',
+  'config.lua',
+  '@ox_core/lib/init.lua'
+}
+
+
+client_scripts {
+  'client/modules/utils.lua',
+  'client/modules/state.lua',
+  'client/modules/ui.lua',
+  'client/modules/player.lua',
+  'client/modules/vehicle.lua',
+  'client/modules/notifications.lua',
+  'client/client.lua'
+}
+
+server_scripts {
+  'server/modules/notifications.lua',
+  'server/server.lua'
+}
+
+files {
+	'web/build/index.html',
+	'web/build/**/*',
+}
+
+dependencies {
+	'ox_core',
+	'interact-sound',
+  'oxmysql',
+}
